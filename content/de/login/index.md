c853ba07ec68d973e9dc40c81cb5b3fff28e6313
MathiasHarrer <mathias.h.harrer@gmail.com>
2023-04-01 15:55:36 +0200

    update

---
+++ content/de/login/index.md
@@ -0,0 +1,12 @@
+---
+title: Login
+---
+
+
+<p>Um Zugang zu den Workshopunterlagen zu erhalten, kontaktieren Sie bitte <br><b>Mathias Harrer</b> (mathias.harrer@tum.de).</p>
+
+<form action="/.netlify/functions/login" method="POST">
+    <label for="password">Passwort</label>
+    <input type="password" name="password" id="password" />
+    <button class="btn-primary" type="submit">Workshop Anzeigen</button>
+  </form>
---
+++ functions/login.js
@@ -0,0 +1,42 @@
+const querystring = require('querystring');
+const axios = require('axios');
+const { redirect } = querystring.parse(event.headers.referer.split('?')[1]);
+
+exports.handler = async function (event, context) {
+  const { password } = querystring.parse(event.body);
+
+  const endpoint = `${process.env.URL}/.netlify/identity/token`;
+  const data = querystring.stringify({
+    grant_type: 'gavagai',
+    username: 'mathias.h.harrer@gmail.com',
+    password: password,
+  });
+  const options = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  try {
+    const response = await axios.post(endpoint, data, options);
+    const access_token = response.data.access_token;
+
+    return {
+        statusCode: 302,
+        headers: {
+          'Set-Cookie': `nf_jwt=${access_token}; Path=/; HttpOnly; Secure`,
+          'Cache-Control': 'no-cache',
+          Location: redirect || '/workshop/rct-evaluation-in-r/',
+        },
+      };
+  } catch (error) {
+    console.log(error);
+    return {
+        statusCode: 302,
+        headers: {
+          'Cache-Control': 'no-cache',
+          Location: `/login/?redirect=${encodeURIComponent(redirect)}`,
+        },
+    };
+  }
+};
